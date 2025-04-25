a1a67ea3956115b897d2e828f8fcdca92ca8c506
Thetsmr <thetsmr@gmail.com>
2025-04-25 19:35:23 +0200

    Indent code & Update Headers

--- inc/config.class.php
+++ inc/config.class.php
@@ -20,8 +20,8 @@
  * along with Behaviors. If not, see <http://www.gnu.org/licenses/>.
  *
  * @package   behaviors
- * @author    Remi Collet, Nelly Mahu-Lasson
- * @copyright Copyright (c) 2010-2022 Behaviors plugin team
+ * @author    Infotel, Remi Collet, Nelly Mahu-Lasson
+ * @copyright Copyright (c) 2018-2025 Behaviors plugin team
  * @license   AGPL License 3.0 or (at your option) any later version
  * http://www.gnu.org/licenses/agpl-3.0-standalone.html
  * @link      https://github.com/InfotelGLPI/behaviors/
@@ -273,6 +273,10 @@ class PluginBehaviorsConfig extends CommonDBTM
     }
 
 
+    /**
+     * @param $item
+     * @return false
+     */
     static function showConfigForm($item)
     {
         $yesnoall = [
--- inc/itilfollowup.class.php
+++ inc/itilfollowup.class.php
@@ -1,75 +1,81 @@
 <?php
 /**
- -------------------------------------------------------------------------
+ * -------------------------------------------------------------------------
+ *
+ * LICENSE
+ *
+ * This file is part of Behaviors plugin for GLPI.
+ *
+ * Behaviors is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Behaviors is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Behaviors. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package   behaviors
+ * @author    Infotel, Remi Collet, Nelly Mahu-Lasson
+ * @copyright Copyright (c) 2018-2025 Behaviors plugin team
+ * @license   AGPL License 3.0 or (at your option) any later version
+ * http://www.gnu.org/licenses/agpl-3.0-standalone.html
+ * @link      https://github.com/InfotelGLPI/behaviors/
+ * @link      http://www.glpi-project.org/
+ * @since     2010
+ *
+ * --------------------------------------------------------------------------
+ */
 
- LICENSE
+class PluginBehaviorsITILFollowup
+{
 
- This file is part of Behaviors plugin for GLPI.
+    /**
+     * @param ITILFollowup $fup
+     * @return void
+     */
+    static function beforeAdd(ITILFollowup $fup)
+    {
+        $ticket = new Ticket();
+        $config = PluginBehaviorsConfig::getInstance();
+        if ($ticket->getFromDB($fup->input['items_id'])
+            && $fup->input['itemtype'] == 'Ticket') {
+            if ($config->getField('addfup_updatetech')
+                && Session::haveRight('ticket', UPDATE)) {
+                $ticket_user = new Ticket_User();
+                $ticket_user->getFromDBByCrit([
+                    'tickets_id' => $ticket->getID(),
+                    'type' => CommonITILActor::ASSIGN
+                ]);
 
- Behaviors is free software: you can redistribute it and/or modify
- it under the terms of the GNU Affero General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
+                if (isset($ticket_user->fields['users_id'])
+                    && ($ticket_user->fields['users_id'] <> Session::getLoginUserID())) {
+                    $group_ticket = new Group_Ticket();
+                    $group_ticket->getFromDBByCrit([
+                        'tickets_id' => $ticket->getID(),
+                        'type' => CommonITILActor::ASSIGN
+                    ]);
 
- Behaviors is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- GNU Affero General Public License for more details.
+                    $usergroup = Group_User::getGroupUsers($group_ticket->fields['groups_id']);
+                    $users = [];
+                    foreach ($usergroup as $user) {
+                        $users[$user['id']] = $user['id'];
+                    }
 
- You should have received a copy of the GNU Affero General Public License
- along with Behaviors. If not, see <http://www.gnu.org/licenses/>.
-
- @package   behaviors
- @author    Nelly Mahu-Lasson
- @copyright Copyright (c) 2022 Behaviors plugin team
- @license   AGPL License 3.0 or (at your option) any later version
-            http://www.gnu.org/licenses/agpl-3.0-standalone.html
- @link      https://github.com/InfotelGLPI/behaviors/
- @link      http://www.glpi-project.org/
- @since     2022
-
- --------------------------------------------------------------------------
-*/
-
-class PluginBehaviorsITILFollowup {
-
-   static function beforeAdd(ITILFollowup $fup) {
-
-      $ticket = new Ticket();
-      $config = PluginBehaviorsConfig::getInstance();
-      if ($ticket->getFromDB($fup->input['items_id'])
-         && $fup->input['itemtype'] == 'Ticket') {
-
-         if ($config->getField('addfup_updatetech')
-             && Session::haveRight('ticket', UPDATE)) {
-
-            $ticket_user      = new Ticket_User();
-            $ticket_user->getFromDBByCrit(['tickets_id' => $ticket->getID(),
-                                           'type'       => CommonITILActor::ASSIGN]);
-
-            if (isset($ticket_user->fields['users_id'])
-                && ($ticket_user->fields['users_id'] <> Session::getLoginUserID())) {
-               $group_ticket      = new Group_Ticket();
-               $group_ticket->getFromDBByCrit(['tickets_id' => $ticket->getID(),
-                                               'type'       => CommonITILActor::ASSIGN]);
-
-               $usergroup = Group_User::getGroupUsers($group_ticket->fields['groups_id']);
-               $users = [];
-               foreach ($usergroup as $user) {
-                  $users[$user['id']] = $user['id'];
-               }
-
-               if (!in_array( Session::getLoginUserID(), $users)) {
-                  $ticket_user = new Ticket_User();
-                  $ticket_user->add(['tickets_id' => $ticket->getID(),
-                                     'users_id'   => Session::getLoginUserID(),
-                                     'type'       => CommonITILActor::ASSIGN]);
-               }
+                    if (!in_array(Session::getLoginUserID(), $users)) {
+                        $ticket_user = new Ticket_User();
+                        $ticket_user->add([
+                            'tickets_id' => $ticket->getID(),
+                            'users_id' => Session::getLoginUserID(),
+                            'type' => CommonITILActor::ASSIGN
+                        ]);
+                    }
+                }
             }
-         }
-      }
-   }
-
-
-
+        }
+    }
 }
